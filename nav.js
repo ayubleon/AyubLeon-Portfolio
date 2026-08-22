c23959933e672987d595ea1fc959b9ba78c86922
Ayub Leon <ayubleon9@gmail.com>
2026-08-22 18:01:17 +0300

    Bring whoosh volume back up to 0.25

--- nav.js
+++ nav.js
@@ -117,7 +117,7 @@
       else if (ready) ready.then(start);
     };
   }
-  var playWhoosh = makeSoundPlayer('sounds/whoosh.wav', 0.12);
+  var playWhoosh = makeSoundPlayer('sounds/whoosh.wav', 0.25);
   var playClick = makeSoundPlayer('sounds/click.mp3', 0.6);
 
   function copyEmail(e) {
